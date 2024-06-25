f32e57aa96d9e78699e9075ba224258a0f2c7a60
devaryakjha <dev.jha.arya@gmail.com>
2024-06-25 16:02:21 +0530

    feat: Add raw player exports to video_player_extended.dart
    This commit adds the necessary exports for the raw player to the video_player_extended.dart file. By exporting the RawVideoPlayerController and RawVideoPlayer classes, the raw player functionality can be easily accessed and utilized. This enhancement improves the overall functionality and flexibility of the video player extended library.

---
+++ lib/raw_player.dart
@@ -0,0 +1,2 @@
+export 'src/players/raw/index.dart'
+    show RawVideoPlayerController, RawVideoPlayer;
--- lib/video_player_extended.dart
+++ lib/video_player_extended.dart
@@ -2,11 +2,10 @@ library video_player_extended;
 
 export 'package:video_player/video_player.dart' show VideoPlayerController;
 
+export 'raw_player.dart';
 export 'src/common/index.dart'
     show
         ReadControllerExtension,
         WatchControllerExtension,
         SelectControllerExtension,
         ThumbnailConfig;
-export 'src/players/raw/index.dart'
-    show RawVideoPlayerController, RawVideoPlayer;
